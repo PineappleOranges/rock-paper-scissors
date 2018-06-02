bc001c545e61eca294d03abf0dbfe0bc4c2afc61
Kiel Bath <38203520+PineappleOranges@users.noreply.github.com>
2018-06-01 19:42:35 -0500

    Add message for player and computer hand

--- game.functions.js
+++ game.functions.js
@@ -8,20 +8,28 @@ const roundResults = document.createElement('h4');
 roundResults.textContent = "Player Score: " + playerScore + "  Computer Score: " + computerScore;
 content.appendChild(roundResults);
 
+const roundHands = document.createElement('h1'); 
+roundHands.textContent = "Select your hand. First to 5 points is the winner!"
+content.appendChild(roundHands);
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
 	button.addEventListener('click', (e) => {
 		playerSelection = button.id;
 		computerSelection = computerPlay();
-		game();
+		roundHands.textContent = game();
+		content.appendChild(roundHands);
 		roundResults.textContent = "Player Score: " + playerScore + "  Computer Score: " + computerScore;
 		content.appendChild(roundResults);
+		if (playerScore == 0 && computerScore == 0) {
+			roundHands.textContent = "Select your hand. First to 5 points is the winner!"
+			content.appendChild(roundHands);
+		}
 	});
 });
 
 function game() {
-	playRound(playerSelection, computerSelection);
+	const roundResult = playRound(playerSelection, computerSelection);
 	if (playerScore == 5) {
 		alert("You win!");
 		playerScore = 0;
@@ -31,24 +39,28 @@ function game() {
 		playerScore = 0;
 		computerScore = 0;
 	}
+	return roundResult;
 }
 
 function playRound(playerSelection, computerSelection) {
+	let roundResult = null;
+
 	if (playerSelection == computerSelection) {
-		console.log("Draw!");
+		roundResult = "Draw!";
 	}	else if (playerSelection == "ROCK" && computerSelection == "SCISSORS") {
 		playerScore ++;
-		console.log("You Win!");
+		roundResult = "You Win! " + playerSelection + " beats " + computerSelection + ".";
 	}	else if (playerSelection == "PAPER" && computerSelection == "ROCK") {
 		playerScore ++;
-		console.log("You Win!");
+		roundResult = "You Win! " + playerSelection + " beats " + computerSelection + ".";
 	}	else if (playerSelection == "SCISSORS" && computerSelection == "PAPER") {
 		playerScore ++;
-		console.log("You Win!");
+		roundResult = "You Win! " + playerSelection + " beats " + computerSelection + ".";
 	}	else {
 		computerScore ++;
-		console.log("You Lose! " + computerSelection + " beats " + playerSelection + "."); 
+		roundResult = "You Lose! " + computerSelection + " beats " + playerSelection + "."; 
 	}
+	return roundResult;
 }
 
 function computerPlay() {
